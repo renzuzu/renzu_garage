fcb96a9e24a4e16e6259364b6e3f6ee027e9bbfb
renzuzu <82306584+renzuzu@users.noreply.github.com>
2021-07-24 20:04:31 +0800

    new sql update (required)

--- renzu_garage/garage.sql
+++ renzu_garage/garage.sql
@@ -6,3 +6,8 @@ ADD impound int(1) NOT NULL DEFAULT 0;
 
 ALTER TABLE owned_vehicles
 ADD stored int(1) NOT NULL DEFAULT 0;
+
+ALTER TABLE owned_vehicles
+ADD park_coord LONGTEXT NULL DEFAULT '[]';
+ALTER TABLE owned_vehicles
+ADD isparked int(1) NULL DEFAULT 0;
