932c3bdafbddd56c57a737bb79dd343d95f18ed7
renzuzu <82306584+renzuzu@users.noreply.github.com>
2021-11-25 19:41:30 +0800

    feat(conf): notify config

--- conf/main.lua
+++ conf/main.lua
@@ -1,12 +1,7 @@
 Config = {}
 Config.Locale = "en" -- en,es,de or any language support
 --FRAMEWORK
-<<<<<<< Updated upstream
 Config.framework = 'ESX' -- ESX, QBCORE
-=======
-Config.framework = 'QBCORE' -- ESX, QBCORE
-
->>>>>>> Stashed changes
 --GENERAL SETTING
 Config.Mysql = 'oxmysql' -- "ghmattisql", "mysql-async", "oxmysql"
 Config.use_RenzuCustoms = false -- Use renzu_customs getter and setter for Vehicle Properties
